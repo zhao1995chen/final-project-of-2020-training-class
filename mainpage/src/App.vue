d35d0d56f4ccbdfe5171495e56b2d3805843ec09
zhao1995chen <zhao1995.chen@gmail.com>
2020-08-14 14:10:15 +0800

    add shoplist and images to temp path

--- mainpage/src/App.vue
+++ mainpage/src/App.vue
@@ -12,6 +12,7 @@
 <script>
 import L from 'leaflet';
 import shop from './assets/image/location/png/location(13).png';
+import ShopItems from './assets/json/Items.json';
 
 const axios = require('axios');
 
@@ -19,6 +20,7 @@ export default {
   name: 'App',
   data: () => ({
     postOffices: [],
+    shopItems: [],
     OSMap: {},
   }),
   async created() {
@@ -28,6 +30,7 @@ export default {
     // eslint-disable-next-line
     console.log(this.postOffices);
     await this.initMap();
+    await this.getShopItems();
     await this.addMarkers();
   },
   methods: {
@@ -77,13 +80,40 @@ export default {
             iconUrl: shop,
             iconSize: [75, 75],
           }),
-        }).bindPopup(`<p><strong style="font-size: 20px;">${po.name}</strong></p>`).addTo(this.OSMap);
+        }).bindPopup(`
+          <p><strong style="font-size: 20px;">${po.name}</strong></p>
+          ${this.showShop()}
+          <div class="d-flex justify-content-center">
+            <button class="btn btn-primary">購買</button>
+          </div>
+          `).addTo(this.OSMap);
       });
     },
     randomLocation() {
       const randomIndex = Math.floor(Math.random() * this.postOffices.length + 1);
       const randomLocation = this.postOffices[randomIndex];
       this.OSMap.flyTo(new L.LatLng(randomLocation.latitude, randomLocation.longitude));
+    },
+    getShopItems() {
+      this.shopItems = Object.keys(ShopItems).map((key) => ShopItems[key]);
+      console.log(this.shopItems);
+    },
+    showShop() {
+      let shopList = '';
+      this.shopItems.forEach((item) => {
+        shopList += `
+        <div class="d-flex row">
+            <div class="itemContent col-3"><img src="assets/image/game/png/game(${item.iid}).png" width="50" height="50"></div>
+            <div class="itemContent col-3"><p style="font-size: 1rem;">${item.name}</p></div>
+            <div class="itemContent col-6 d-flex justify-content-center">
+              <button class="col-4 btn btn-outline-secondary">-</button>
+              <span class="col-4 align-self-center text-center">0</span>
+              <button class="col-4 btn btn-outline-secondary">+</button>
+            </div>
+          </div>
+        </div>`;
+      });
+      return shopList;
     },
   },
 };
@@ -92,12 +122,22 @@ export default {
 <style lang="scss">
 @import 'bootstrap/scss/bootstrap';
 
-h1, h2, h3, h4, h5, h6, p {
+h1, h2, h3, h4, h5, h6, p, button {
   font-family: '微軟正黑體';
+  text-align: center;
 }
 
 #map {
   height: 100vh;
   position: relative;
+}
+
+.itemContent {
+  display: inline-block;
+  margin: auto;
+}
+
+.leaflet-popup-content {
+  width: 350px;
 }
 </style>
